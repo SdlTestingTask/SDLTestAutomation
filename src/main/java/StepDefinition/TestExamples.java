78848f635ffb512c27b0b72a1c5c4b58db0247ec
SdlTestingTask <anand.manyam07@gmail.com>
2021-03-15 17:19:06 +0530

    updated the code in TestExample java  class

--- src/main/java/StepDefinition/TestExamples.java
+++ src/main/java/StepDefinition/TestExamples.java
@@ -1,9 +1,11 @@
 package StepDefinition;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -23,7 +25,7 @@ public class TestExamples{
 	@Given("^user is open login page$")
 	public void user_is_open_login_page() {
 		
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Selenium\\New folder\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Selenium\\New folder\\chromedriver.exe");// here we need to give chrome driver path
 		
 		driver = new ChromeDriver();
 		
@@ -49,52 +51,55 @@ public class TestExamples{
 	@Then("^user open projects page$")
 	public void user_open_projects_page() {
 		
-		driver.findElement(By.xpath("//div[@class='x-segmented-button sdl-nav x-box-item x-toolbar-item x-segmented-button-horizontal x-segmented-button-default']/a[3]")).click();
+		driver.findElement(By.xpath("//a[contains(@class, 'x-btn') and count(*//text()[.='Projects']) > 0]")).click();
 		
 	}
 	
 	@Then("^user click on the New Project button$")
 	public void user_click_on_the_New_Project_button() {
 		
-		driver.findElement(By.xpath("//div[@class='x-box-inner x-box-scroller-body-horizontal x-scroller']/div/a[7]")).click();
+		driver.findElement(By.xpath("//a[contains(@class, 'x-btn') and count(*//text()[.='New Project']) > 0]")).click();
 		
 	}
 	
 	@Then("^select general details$")
 	public void select_general_details() {
 		
-		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
 		
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		
         driver.findElement(By.xpath("//input[@name='name']")).sendKeys("New Test Projects");
 		
-		WebElement element = driver.findElement(By.xpath("//div[@id='folderscombobox-1575-trigger-picker']"));
+        WebElement element = driver.findElement(By.xpath("//input[@name='location'] [@placeholder='Select a location']/../following-sibling::div"));
 		
-		JavascriptExecutor executor = (JavascriptExecutor)driver;
+        JavascriptExecutor executor = (JavascriptExecutor)driver;
 		executor.executeScript("arguments[0].click();", element);
         
-		driver.findElement(By.xpath("//ul[@id='folderscombobox-1575-picker-listEl']/ul/li[5]")).click();
+		driver.findElement(By.xpath("//li[text()='HP']")).click();
 		
-		driver.findElement(By.xpath("//div[@id='languagescombobox-1585-trigger-picker']")).click();
+		driver.findElement(By.xpath("//input[@name='projectSourceLanguage']/../following-sibling::div")).click();
 		
-		driver.findElement(By.xpath("//ul[@id='languagescombobox-1585-picker-listEl']/li[1]")).click();
+	    driver.findElement(By.xpath("//i[@data-qtip='en-us']/..")).click();
 		
-		driver.findElement(By.xpath("//div[@id='languagestag-1586-trigger-picker']")).click();
+		driver.findElement(By.xpath("//span[text()='Target Languages']/../../following-sibling::div[1]/div/div[2]")).click();
+	   
+		driver.findElement(By.xpath("//i[@data-qtip='ur-IN']/..")).click();
 		
-		driver.findElement(By.xpath("//ul[@id='languagestag-1586-picker-listEl']/ul/li[539]")).click();
+		driver.findElement(By.xpath("//span[text()='File Type Configuration']/../../following-sibling::div[1]/div/div[2]")).click();
+	    
 		
-		driver.findElement(By.xpath("//div[@id='strategycombobox-1587-trigger-picker']")).click();
+		List<WebElement> ele = driver.findElements(By.xpath("//div[text()='Filter']/parent::div/parent::ul[@class='x-list-plain']/li"));
+																																																											
+		WebElement element_1 = ele.get(1);
+		element_1.click();
 		
-		driver.findElement(By.xpath("//div[@id='strategycombobox-1587-picker']/div[1]/ul/li[1]")).click();
+		driver.findElement(By.xpath("//span[@class='x-btn-wrap x-btn-wrap-link-medium ']/following-sibling::input")).sendKeys("C:\\Users\\Anand\\Desktop\\AnandBackup\\SDL\\version.txt");// we need to give path to browse a file this text file is available in project so you can give that file path
 		
-		driver.findElement(By.xpath("//input[@id='filefield-1620-button-fileInputEl']")).sendKeys("C:\\Users\\Anand\\eclipse-workspace\\TestAutomation\\version.txt");
-		
-		driver.findElement(By.xpath("//a[@id='button-1646']")).click();
+		driver.findElement(By.xpath("//span[text()='Next']/../../..")).click();
 		
 		
-		
-	}
+		}
 	
 	@Then("^select the translate engine$")
 	public void select_the_translate_engine() {
@@ -103,11 +108,13 @@ public class TestExamples{
 		
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		
-        driver.findElement(By.xpath("//div[@id='newactioncomboboxwithfilter-1656-trigger-picker'][@class='x-form-trigger x-form-trigger-default x-form-arrow-trigger x-form-arrow-trigger-default ']")).click();
+		driver.findElement(By.xpath("//span[text()='Translation Engine'][@class='x-form-item-label-text']/../../following-sibling::div/div/div[2]")).click();
+	
+		driver.findElement(By.xpath("//div[contains(text(),'HP_TranslationEngine')]/..")).click();
 		
-		driver.findElement(By.xpath("//li[@id='ext-1225']")).click();
-		
-		driver.findElement(By.xpath("//a[@id='button-1646']")).click();
+		WebElement ele_1 = driver.findElement(By.xpath("//span[text()='Next']/../../.."));
+		JavascriptExecutor exe = (JavascriptExecutor)driver;
+		exe.executeScript("arguments[0].click();", ele_1);
 		
 		
 	}
@@ -115,17 +122,15 @@ public class TestExamples{
 	@Then("^select the pricing model$")
 	public void select_the_pricing_model() {
 		
-        driver.manage().deleteAllCookies();
-		
 		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
 		
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		
-        driver.findElement(By.xpath("//div[@id='newactioncomboboxwithfilter-1681-trigger-picker']")).click();
+        driver.findElement(By.xpath("//span[text()='Pricing Model'][@class='x-form-item-label-text']/../../following-sibling::div[1]/div/div[2]")).click();
 		
-		driver.findElement(By.xpath("//div[@id='newactioncomboboxwithfilter-1681-picker']/div[1]/ul/li[2]")).click();
+		driver.findElement(By.xpath("//div[contains(text(),'root pricing model')]/..")).click();
 		
-		driver.findElement(By.xpath("//a[@id='button-1646']")).click();
+		driver.findElement(By.xpath("//span[text()='Next']/../../..")).click();
 	}
 	
 	@Then("^select the workflow$")
@@ -137,12 +142,12 @@ public class TestExamples{
 		
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		
-        driver.findElement(By.xpath("//div[@id='newactioncomboboxwithfilter-1700-trigger-picker']")).click();
+        driver.findElement(By.xpath("//span[text()='Workflow'][@class='x-form-item-label-text']/../../following-sibling::div/div/div[2]")).click();
 		
-		driver.findElement(By.xpath("//div[@id='newactioncomboboxwithfilter-1700-picker']/div[1]/ul/li[1]")).click();
+		driver.findElement(By.xpath("//div[contains(text(),'WF-4_CP_2Languages')]/..")).click();
 		
-		driver.findElement(By.xpath("//span[@id='button-1663-btnInnerEl']")).click();
-		Thread.sleep(2000);
+		driver.findElement(By.xpath("//span[text()='Create & Start']/../../..")).click();
+		Thread.sleep(5000);
 		
 		
 	}
